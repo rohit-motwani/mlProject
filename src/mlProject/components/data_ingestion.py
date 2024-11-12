8cd5acfd70a4354dcefe9d664f28f0b970628497
rohit-motwani <122899771+rohit-motwani@users.noreply.github.com>
2024-11-12 08:34:05 +0530

    data ingestion changes

--- src/mlProject/components/data_ingestion.py
+++ src/mlProject/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 from src.mlProject.utils import read_sql_data
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path = os.path.join('artifacts','train.csv')
@@ -24,8 +26,8 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-            df.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            df.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
             
             logging.info("Data Ingestion is Completed")
             
